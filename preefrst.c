c43e44ee5a301a229d873cad9405299da42097bf
Preethi1406 <30458059+Preethi1406@users.noreply.github.com>
2017-07-26 11:15:08 +0530

    Create preefrst.c

---
+++ preefrst.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  if(n>0)
+   {
+     printf("It is positive...");
+   }
+   else if(n<0)
+   {
+     printf("It is Negative...");
+    }
+   else
+   printf("Number is ZERO");
+}
